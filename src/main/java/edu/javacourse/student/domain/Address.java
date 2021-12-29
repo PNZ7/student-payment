c763fe5c5ce548f2c8720be719a121260ffe2069
Oleg <sorokin58.90@mail.ru>
2021-12-29 16:17:36 +0300

    155 Entity binding for the Embeddable class

---
+++ src/main/java/edu/javacourse/student/domain/Address.java
@@ -0,0 +1,57 @@
+package edu.javacourse.student.domain;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+public class Address {
+
+    private String postCode;
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    private Street street;
+    private String building;
+    private String extension;
+    private String appartment;
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public Street getStreet() {
+        return street;
+    }
+
+    public void setStreet(Street street) {
+        this.street = street;
+    }
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(String building) {
+        this.building = building;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public String getAppartment() {
+        return appartment;
+    }
+
+    public void setAppartment(String appartment) {
+        this.appartment = appartment;
+    }
+}
--- src/main/java/edu/javacourse/student/domain/Person.java
+++ src/main/java/edu/javacourse/student/domain/Person.java
@@ -8,6 +8,7 @@ public class Person {
     private String givenName;
     private String patronymic;
     private LocalDate dateOfBirth;
+    private Address address;
 
     public String getSurName() {
         return surName;
@@ -39,5 +40,13 @@ public class Person {
 
     public void setDateOfBirth(LocalDate dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
     }
 }
